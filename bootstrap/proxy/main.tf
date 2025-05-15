65bc56d09f99c93bf4ea8cb68c2488613cccbc27
Felipe Gonzalez <gonzalezz_felipe@hotmail.com>
2025-05-15 17:34:44 -0300

    fix: Pass dns_names manually for dmtr.host

--- bootstrap/proxy/main.tf
+++ bootstrap/proxy/main.tf
@@ -7,6 +7,14 @@ locals {
   proxy_port      = 8080
   proxy_addr      = "0.0.0.0:${local.proxy_port}"
   proxy_labels    = { role = "${local.role}" }
+
+  by_version = flatten([
+    for version in var.versions : "*.${var.network}-v${version}.${var.extension_name}.${var.dns_zone}"
+  ])
+
+  # Add the extra URL to the list of generated URLs
+  dns_names        = var.dns_names != null ? var.dns_names : concat(local.by_version, ["*.${var.extension_name}.${var.dns_zone}"])
+  cert_secret_name = var.environment != null ? "${var.extension_name}-${var.environment}-${var.network}-wildcard-tls" : "${var.extension_name}-${var.network}-wildcard-tls"
 }
 
 // blue - green
@@ -73,6 +81,11 @@ variable "network" {
 variable "versions" {
   type    = list(string)
   default = ["5", "6"]
+}
+
+variable "dns_names" {
+  type    = list(string)
+  default = null
 }
 
 variable "dns_zone" {
